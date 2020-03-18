e6123374f82f3129b1f7afb3aaed19a7c44bc1af
masakiuchida <masaki-uchida@nekokari.co.jp>
2020-03-18 11:29:07 +0900

    check

--- main.py
+++ main.py
@@ -9,19 +9,4 @@ def hello_pubsub(event, context):
     """
     pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
 
-    message = pubsub_message
-    mes = """
-    Title: %s
-    発生時刻: %s
-    発生した事項: %s
-    リソース名: %s
-    詳細: %s
-    """ % (
-        '死活監視　通知',
-        message['receiveTimestamp'],
-        message['jsonPayload']['event_subtype'],
-        message['jsonPayload']['resource']['name'],
-        message['logName']
-    )
-
-    json.dumps(mes)
+    print(pubsub_message)
