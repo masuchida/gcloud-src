f88b03fb376bdfc595b70d22c096eeecfd4579e8
masakiuchida <masaki-uchida@nekokari.co.jp>
2020-03-18 11:14:38 +0900

    check

--- main.py
+++ main.py
@@ -7,21 +7,6 @@ def hello_pubsub(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
 
-    message = pubsub_message
-    mes = """
-    Title: %s
-    発生時刻: %s
-    発生した事項: %s
-    リソース名: %s
-    詳細: %s
-    """ % (
-        '死活監視　通知',
-        message['receiveTimestamp'],
-        message['jsonPayload']['event_subtype'],
-        message['jsonPayload']['resource']['name'],
-        message['logName']
-    )
-
-    print(mes)
+    print(message)
