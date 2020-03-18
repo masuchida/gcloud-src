f8d48d2d3ceb187198e444ea3cb0c158b0d953b2
masakiuchida <masaki-uchida@nekokari.co.jp>
2020-03-18 11:38:54 +0900

    check

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def hello_pubsub(event, context):
     """
     pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
 
-    print(pubsub_message)
+    print(json.dumps(pubsub_message))
