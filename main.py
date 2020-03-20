bebbdb63b3e3b6b91b766cb23117d9d9def1a188
masakiuchida <masaki-uchida@nekokari.co.jp>
2020-03-21 02:49:18 +0900

    2020/03/21 - 2:49

--- main.py
+++ main.py
@@ -31,31 +31,31 @@ def hello_pubsub(event, context):
         start_date = datetime.fromtimestamp(message['started_at'])
         jst = start_date.astimezone(timezone('Asia/Tokyo'))
     elif incident_flag == 'closed':
-        incident_flag = '回復'
-        summary = 'サーバー回復を確認しました。'
-        end_date = datetime.fromtimestamp(message['ended_at'])
-        jst = end_date.astimezone(timezone('Asia/Tokyo'))
+            incident_flag = '回復'
+            summary = 'サーバー回復を確認しました。'
+            end_date = datetime.fromtimestamp(message['ended_at'])
+            jst = end_date.astimezone(timezone('Asia/Tokyo'))
 
     mes = """
-        [info][title]検知: % s[/title]
-        発生時刻: % s
-        監視項目名: % s
-        対象リソース名: % s
-        エラー詳細URL: % s[/info]
+            [info][title]検知: % s[/title]
+            発生時刻: % s
+            監視項目名: % s
+            対象リソース名: % s
+            エラー詳細URL: % s[/info]
         """ % (
-        incident_flag,
-        jst,
-        summary,
-        message['resource_display_name'],
-        message['url']
+            incident_flag,
+            jst,
+            summary,
+            message['resource_display_name'],
+            message['url']
     )
 
     data = {
-        'body': mes
+            'body': mes
     }
 
     headers = {
-        'X-ChatWorkToken': TOKEN
+            'X-ChatWorkToken': TOKEN
     }
 
     requests.post(POST, headers=headers, data=data)
